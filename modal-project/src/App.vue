4d377e87a9dbf3052ad0c9c5b49472ebc588d93e
“Brian <huynh2102@gmail.com>
2023-07-01 00:51:03 +0700

    update code

---
+++ modal-project/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <Modal />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data () {
+    return {
+      title: 'My first Vue App fdf:)'
+    }
+  },
+  methods: {
+    handleClick () {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #62717f;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p> modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop {
+        top: 150px;
+        position: fixed;
+        background: rgb(215, 200, 200);
+        width: 100%;
+        height: 100%;
+    }
+</style>
