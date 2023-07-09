d4f4dc2877231a056204d09d09bc11e3039d2a36
“Brian <huynh2102@gmail.com>
2023-07-09 10:53:40 +0700

    update initial code

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start">play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+export default {
+  name: 'App',
+  components: { Block },
+  data() {
+    return {
+    isPlaying: false,
+    delay: null
+     }
+  }, 
+  methods: {
+    start() {
+      this.deplay = 2000 + Math.random() * 5000,
+      this.isPlaying = true,
+      console.log(this.deplay)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
