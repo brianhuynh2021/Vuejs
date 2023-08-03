b141c61109ae33717e22ef5ce2109f33085d353c
“Brian <huynh2102@gmail.com>
2023-08-03 08:23:05 +0700

    update keyup into ctrl

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,25 +1,26 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <label>Email:</label>
-    <input type="email" required v-model="email">
-    
-    <label>Password</label>
-    <input type="password" required v-model="password">
-    <div v-if="passwordError">  {{ passwordError }}</div>
+    <input type="email" v-model="email" required>
+
+    <label>Password:</label>
+    <input type="password" v-model="password" required>
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
+
     <label>Role:</label>
     <select v-model="role">
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
-    </select> 
+    </select>
 
-    <label>Skills:</label>
-    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <label>Skills (press alt + comma to add):</label>
+    <input type="text" v-model="tempSkill" @keyup.ctrl="addSkill">
     <div v-for="skill in skills" :key="skill" class="pill">
-       <span @click="deleteSkill(skill)"> {{ skill }} </span>
-    </div> 
-     
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
     <div class="terms">
-      <input type="checkbox"  v-model="terms" required>
+      <input type="checkbox" v-model="terms" required>
       <label>Accept terms and conditions</label>
     </div>
 
@@ -27,30 +28,24 @@
       <button>Create an Account</button>
     </div>
   </form>
-
-  <p>Email: {{ email }}</p>
-  <p>Password: {{ password }}</p>
-  <p>Role: {{ role }}</p>
-  <p>Terms accepted {{ terms }}</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      email: 'mario',
+      email: '',
       password: '',
-      role: 'designer',
+      role: 'developer',
       terms: false,
-      names: [],
-      tempSkill: '',
       skills: [],
-      passwordError: ''
+      tempSkill: '',
+      passwordError: null,
     }
   },
   methods: {
-    addSkill(e) {
-      if (e.key === ',' && this.tempSkill){
+    addSkill($event) {
+      if($event.key === ',' && this.tempSkill) {
         if (!this.skills.includes(this.tempSkill)) {
           this.skills.push(this.tempSkill)
         }
@@ -58,91 +53,89 @@ export default {
       }
     },
     deleteSkill(skill) {
-      this.skills = this.skills.filter((item) => {
+      this.skills = this.skills.filter(item => {
         return skill !== item
       })
     },
     handleSubmit() {
       // validate password
-      this.passwordError = this.password.length > 5 ? 
-        '' : 'Password must be at least 6 chars long'
-      
-        if(!this.passwordError){
-          console.log('email: ', this.email)
-          console.log('password: ', this.password)
-          console.log('role: ', this.role)
-          console.log('skills: ', this.skills)
-          console.log('terms accepted: ', this.terms)
-        }
+      this.passwordError = this.password.length > 5 ?
+        '' : 'Password must be at least 6 characters long'
+
+      if (!this.passwordError) {
+        // make request to database to save user
+        console.log('email: ', this.email)
+        console.log('password: ', this.password)
+        console.log('role: ', this.role)
+        console.log('skills: ', this.skills)
+        console.log('terms accepted: ', this.terms)
+      }
     }
   }
 }
 </script>
 
 <style>
-    form {
-        max-width: 420px;
-        margin: 30px auto;
-        background: white;
-        text-align: left;
-        padding: 40px;
-        border-radius: 10px;
-    }
-
-    label {
-        color: #aaa;
-        display: inline-block;
-        margin: 25px 0 15px;
-        font-size: 0.6em;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        font-weight: bold;
-    }
-
-    input, select{
-        display: block;
-        padding: 10px 6px;
-        width: 100%;
-        box-sizing: border-box;
-        border: none;
-        border-bottom: 1px solid #ddd;
-        color: #555;
-    }
-    input[type="checkbox"]{
-        display: inline-block;
-        width: 16px;
-        margin: 0 10px 0 0;
-        top: 2px;
-        position: relative;
-    }
-
-    .pill {
-      display: inline-block;
-      margin: 20px  10px 0 0;
-      padding: 6px 12px;
-      background: #eee;
-      border-radius: 20px;
-      font-size: 12px;
-      letter-spacing: 1px;
-      font-weight: bold;
-      color: #777;
-      cursor: pointer;
-    }
-
-    button {
-      background: #0b6dff;
-      border: 0;
-      padding: 10px 20px;
-      color: white;
-      border-radius: 20px;
-    }
-    .submit {
-      text-align: center;
-    }
-    .error {
-      color: #ff0062;
-      margin-top: 10px;
-      font-size: 0.8em;
-      font-weight: bold;
-    }
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+  button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+  }
+  .submit {
+    text-align: center;
+  }
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+  }
 </style>
