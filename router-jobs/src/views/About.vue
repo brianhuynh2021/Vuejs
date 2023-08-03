2b828c29161cfac352cef1f798d4d7d19b2484b7
“Brian <huynh2102@gmail.com>
2023-08-03 15:24:52 +0700

    implement components

---
+++ router-jobs/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>lorem 10013</p>
+    <p>lorem 1001878</p>
+  </div>
+</template>
---
+++ router-jobs/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p> lorem100  </p>
+    <p> lorem101  </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
